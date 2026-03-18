c3b3de1583da3116ff751839a423c95bf5bc5c1b
Seungjoo Ham <printseungjoo@gmail.com>
2026-03-19 03:15:09 +0900

    style: global style과 색상 지정하기

---
+++ src/styles/theme.tsx
@@ -0,0 +1,16 @@
+export const theme = {
+    colors: {
+        deepPlum: '#37104B',
+        royalPurple: '#4D2561',
+        mutedViolet: '#683881',
+        midnightPurple: '#250A2A',
+        coolGray: '#D9D9D9',
+        lightWhite: '#FFFFFF',
+        dustyPurple: '#623E74',
+        softLavender: '#866199',
+        mutedRose: '#94455C',
+        warmTaupe: '#9E5D5D',
+        softYellow: '#FEE95E',
+        paleLavender: '#B08BC2'
+    }
+}
