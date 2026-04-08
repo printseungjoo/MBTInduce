0fcb667620a9f2f5b9137c789415e167ee67dcd8
JibeomRyu <126549448+JibeomRyu@users.noreply.github.com>
2026-04-08 16:34:06 +0900

    Revert "Backend-Main Chat"

--- prisma/migrations/20260407063143_add_mbti_slider_weights/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."MbtiPreference" ADD COLUMN     "decisionWeight" INTEGER NOT NULL DEFAULT 50,
-ADD COLUMN     "energyWeight" INTEGER NOT NULL DEFAULT 50,
-ADD COLUMN     "informationWeight" INTEGER NOT NULL DEFAULT 50,
-ADD COLUMN     "lifestyleWeight" INTEGER NOT NULL DEFAULT 50;
