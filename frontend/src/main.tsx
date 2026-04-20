bd8f1c574d340a7944b2c7f706c8892740f76a45
Seungjoo Ham <printseungjoo@gmail.com>
2026-04-20 16:06:25 +0900

    refactor: Separate Main Chat Right Screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,15 @@ import App from './App.tsx'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './styles/theme'
 import { GlobalStyle } from './styles/GlobalStyle'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={ theme }>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/ui/template/RightScreen.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+import { useLocation } from 'react-router-dom'
+
+import MainChatRightScreen from '../organisms/MainChatRightScreen';
+
+interface RightScreenProps {
+    eValues: number;
+    sValues: number;
+    fValues: number;
+    pValues: number;
+    setEValues: React.Dispatch<React.SetStateAction<number>>;
+    setSValues: React.Dispatch<React.SetStateAction<number>>;
+    setFValues: React.Dispatch<React.SetStateAction<number>>;
+    setPValues: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const RightScreenStyled = styled.div`
+    width: 20%;
+    max-width: 20%;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.colors.deepPlum};
+    position: fixed;
+    right: 0;
+    top: 0;
+    box-sizing: border-box;
+    padding: 1.5vh 1vw;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    display: flex;
+    flex-direction: column;
+
+    & > * {
+        width: 100%;
+        max-width: 100%;
+        box-sizing: border-box;
+        flex-shrink: 0;
+    }
+`;
+
+export default function RightScreen({ eValues, sValues, fValues, pValues, setEValues, setSValues, setFValues, setPValues }: RightScreenProps) {
+    const location = useLocation()
+
+    return(
+        <RightScreenStyled>
+            {location.pathname === '/' && <MainChatRightScreen eValues = { eValues } sValues = { sValues } fValues = { fValues } pValues = { pValues } setEValues = { setEValues } setSValues = { setSValues } setFValues = { setFValues } setPValues = { setPValues } />}
+        </RightScreenStyled>
+    )
+}
