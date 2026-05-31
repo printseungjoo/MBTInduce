6fed0bb95ea8b7cb46857b2c608058f2eddc9963
Seungjoo Ham <printseungjoo@gmail.com>
2026-05-31 20:20:44 +0900

    fix: Change URL

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -30,15 +30,15 @@ router.get("/google/callback", ensureGoogleOAuthConfigured, (req, res, next) => 
   passport.authenticate("google", (err, user) => {
     if (err) {
       if (err.message === "Account already registered") {
-        return res.redirect("http://localhost:5173/?error=already_registered");
+        return res.redirect("https://mbtinduce.com/?error=already_registered");
       }
       if (err.message === "Account not registered") {
-        return res.redirect("http://localhost:5173/?error=not_registered");
+        return res.redirect("https://mbtinduce.com/?error=not_registered");
       }
-      return res.redirect("http://localhost:5173/login?error=oauth_failed");
+      return res.redirect("https://mbtinduce.com/login?error=oauth_failed");
     }
     if (!user) {
-      return res.redirect("http://localhost:5173/login?error=oauth_failed");
+      return res.redirect("https://mbtinduce.com/login?error=oauth_failed");
     }
     req.login(user, (loginErr) => {
       if (loginErr) {
