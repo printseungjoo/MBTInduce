384319e3d0f7db23ae5820ce60a0e68b04b05cb3
Seungjoo Ham <printseungjoo@gmail.com>
2026-05-31 21:04:00 +0900

    feat: Add Google Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,21 +14,6 @@ declare global {
   }
 }
 
-const gaMeasurementId = import.meta.env.VITE_GA_ID;
-
-if (gaMeasurementId) {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`;
-  document.head.appendChild(script);
-  window.dataLayer = window.dataLayer || [];
-  function gtag(...args: unknown[]) {
-    window.dataLayer.push(args);
-  }
-  gtag('js', new Date());
-  gtag('config', gaMeasurementId);
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
